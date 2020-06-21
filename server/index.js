9bbc74011e9c9a5114ea20031d33f41bcb67adc2
NaoyaShimada708 <n.step708@gmail.com>
2020-06-21 15:53:38 +0900

    de

--- server/index.js
+++ server/index.js
@@ -3,25 +3,38 @@ const express = require('express')
 // mongooseは、mongodbに簡単にアクセスするためのフレームワーク
 const mongoose = require('mongoose')
 // 大事な情報が入っているのでgitignoreに設定
-const config = require('./config/dev')
+const config = require('./config/index')
 const FakeDb = require('./fake-db')
 
 const productRoutes = require('./routes/products')
+const path = require('path')
 
 mongoose.connect(config.DB_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 }).then(
   () => {
-    const fakeDb = new FakeDb()
-    fakeDb.initDb()
+    if (process.env.NODE_ENV !== 'production') {
+      const fakeDb = new FakeDb()
+      // fakeDb.initDb()
+    }
   }
 )
 
 const app = express()
 
-app.use('/api/v1/products',productRoutes)
+app.use('/api/v1/products', productRoutes)
 
+if (process.env.NODE_ENV === 'production') {
+  const appPath = path.join(__dirname, '..', 'dist', 'reservation-app')
+  app.use(express.static(appPath))
+  app.get("#", function (req, res) {
+    res.sendFile(path.resolve(appPath, 'index.html'))
+  })
+}
+
+// serverディレクトリの一個上の階層のdistをappPathとする
+// /api/v1/products以外のパスが来たらindex.htmlを返す
 // getでエンドポイントが/prductsのリクエストが来たら{}内を返す。functionは、コールバック関数。
 // app.get('/products', function (req, res) {
 //   res.json({'success': true})
