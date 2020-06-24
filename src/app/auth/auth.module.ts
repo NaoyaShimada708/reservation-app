708f3a92d275b353fdb420f42bb6c9bb33e061b2
NaoyaShimada708 <n.step708@gmail.com>
2020-06-24 23:46:45 +0900

    de

---
+++ src/app/auth/auth.module.ts
@@ -0,0 +1,38 @@
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {LoginComponent} from "./login/login.component";
+import {RegisterComponent} from "./register/register.component";
+import {AuthComponent} from "./auth.component";
+import {RouterModule, Routes} from "@angular/router";
+import {ProductComponent} from "../product/product.component";
+import {ProductListComponent} from "../product/product-list/product-list.component";
+import {ProductDetailComponent} from "../product/product-detail/product-detail.component";
+import {AuthService} from "./auth.service";
+
+
+const routes: Routes = [
+  {
+    path: 'auth', component: AuthComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {path: 'register', component: RegisterComponent},
+    ]
+  }
+];
+
+@NgModule({
+  declarations: [
+    LoginComponent,
+    RegisterComponent,
+    AuthComponent,
+  ],
+  imports: [
+    RouterModule.forChild(routes),
+    CommonModule
+  ],
+  providers: [
+    AuthService,
+  ]
+})
+export class AuthModule {
+}
